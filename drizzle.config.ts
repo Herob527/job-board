60cd665044320b1f45c59ea19f7ddf72ba207552
Herob527 <szymon.wrzos@onet.pl>
2026-08-20 11:56:45 +0200

    Working out drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "drizzle-kit";
+
+const dbUrl =
+	process.env.DATABASE_URL ||
+	(() => {
+		throw new Error("DATABASE_URL is not defined");
+	})();
+
+export default defineConfig({
+	out: "./drizzle",
+	schema: "./src/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: dbUrl,
+	},
+});
