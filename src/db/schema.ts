cb525841531f1b6b5b639687562c5fcc088b393b
Herob527 <szymon.wrzos@onet.pl>
2026-08-20 13:30:02 +0200

    Working out schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,15 @@
 import { sql } from "drizzle-orm";
 import { uuid, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable("user", {
+export const users = pgTable("User", {
 	id: uuid().primaryKey().default(sql`uuidv7()`),
 	name: varchar("name", { length: 255 }).notNull(),
 	surname: varchar("surname", { length: 255 }),
 	country: varchar("country", { length: 255 }),
 	email: varchar("email", { length: 255 }).notNull(),
 	password: varchar("password", { length: 255 }).notNull(),
+});
+
+export const candidate = pgTable("Candidate", {
+	id: uuid().primaryKey(),
 });
