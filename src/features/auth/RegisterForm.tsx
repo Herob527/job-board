784b98c8553607198952e4a81788eecedc26cd14
Herob527 <szymon.wrzos@onet.pl>
2026-08-22 12:52:23 +0200

    Added basic register form boilerpalte

---
+++ src/features/auth/RegisterForm.tsx
@@ -0,0 +1,30 @@
+import { useForm } from "@tanstack/preact-form";
+import { registerSchema } from "./schema";
+import type z from "zod";
+
+type RegisterFormData = z.infer<typeof registerSchema>;
+
+const RegisterForm = () => {
+	const formHandler = useForm({
+		defaultValues: {} as RegisterFormData,
+		validators: { onChange: registerSchema },
+		onSubmit: (data) => {
+			console.log(data.value);
+		},
+		onSubmitInvalid: (data) => {
+			console.error(data);
+		},
+	});
+
+	return (
+		<form
+			onSubmit={(ev) => {
+				ev.preventDefault();
+				formHandler.handleSubmit();
+			}}
+			class="inline-flex flex-col gap-2 border border-amber-400 px-4 py-2 rounded-sm"
+		></form>
+	);
+};
+
+export default RegisterForm;
