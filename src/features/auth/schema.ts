e84ab3ccd3975610036369bba8b936e8d1faf648
Herob527 <szymon.wrzos@onet.pl>
2026-08-22 11:45:07 +0200

    Working on register schema

--- src/features/auth/schema.ts
+++ src/features/auth/schema.ts
@@ -10,3 +10,24 @@ export const loginSchema = z.object({
 		.min(8, { message: "Password must be at least 8 characters long" })
 		.max(40, { message: "Password can be at most 40 characters long" }),
 });
+
+export const registerSchema = loginSchema
+	.extend({
+		name: z
+			.string()
+			.min(3, { message: "Name must be at least 3 characters long" })
+			.max(40, { message: "Name can be at most 40 characters long" }),
+		surname: z
+			.string()
+			.min(3, { message: "Surname must be at least 3 characters long" })
+			.max(40, { message: "Surname can be at most 40 characters long" }),
+		country: z
+			.string()
+			.min(3, { message: "Country must be at least 3 characters long" })
+			.max(40, { message: "Country can be at most 40 characters long" }),
+		confirmPassword: z
+			.string()
+			.min(8, { message: "Password must be at least 8 characters long" })
+			.max(40, { message: "Password can be at most 40 characters long" }),
+	})
+	.refine((form) => form.password === form.confirmPassword);
