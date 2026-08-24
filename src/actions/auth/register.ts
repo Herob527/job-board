eb4501de342cddc9d827455144d3ecbe3c2ab825
Herob527 <szymon.wrzos@onet.pl>
2026-08-24 22:04:27 +0200

    Working out registration

--- src/actions/auth/register.ts
+++ src/actions/auth/register.ts
@@ -1,9 +1,27 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { registerSchema } from "#/features/auth/schema";
+import { users } from "#/db/schema";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
 
 export default defineAction({
   input: registerSchema,
-  handler: (input, context) => {
-    console.log(input);
+  handler: async (input, context) => {
+    const { db } = context.locals;
+
+    if (input.registerAs === "company") {
+      console.log(input.companyName);
+      console.log(input.locations);
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(input.password, 10);
+
+    const user = await db.insert(users).values({
+      email: input.email,
+      name: input.name,
+      surname: input.surname,
+      password: hashedPassword,
+      roles: ["candidate"],
+    });
   },
 });
