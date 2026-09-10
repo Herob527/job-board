b3571a37c19cff9abf178df700a9d0fcc6eb3256
Herob527 <szymon.wrzos@onet.pl>
2026-09-10 21:30:13 +0200

    Working on form

--- src/features/job-offer/create/CreateJobOffer.tsx
+++ src/features/job-offer/create/CreateJobOffer.tsx
@@ -1,9 +1,43 @@
+import { actions } from "astro:actions";
+import { useForm } from "@tanstack/preact-form";
+import { useMutation } from "@tanstack/preact-query";
+import type z from "zod";
+import withQuery from "#/features/common/withQuery";
+import jobSchema from "./schema";
+
 interface Props {
   companyId: string;
 }
 
+type JobOfferData = z.infer<typeof jobSchema>;
+
 const CreateJobOffer = ({ companyId }: Props) => {
-  return <form>Job offer</form>;
+  const { mutate } = useMutation({
+    mutationFn: (data: JobOfferData) =>
+      actions.jobOffer.create({
+        ...data,
+        companyId,
+      }),
+  });
+  const formHandler = useForm({
+    validators: { onChange: jobSchema },
+    onSubmit: (data) => mutate({ ...data.value, companyId }),
+
+    onSubmitInvalid: (data) => {
+      console.error(data);
+    },
+  });
+  return (
+    <form
+      onSubmit={(ev) => {
+        ev.preventDefault();
+        formHandler.handleSubmit();
+      }}
+      class="inline-flex flex-col gap-2 border border-amber-400 px-4 py-2 rounded-sm"
+    >
+      Job offer
+    </form>
+  );
 };
 
-export default CreateJobOffer;
+export default withQuery(CreateJobOffer);
