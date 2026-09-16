a77ff018ec866a055a6aa8c391aba82dcb0f1cf7
Herob527 <szymon.wrzos@onet.pl>
2026-09-16 17:47:28 +0200

    Working on list field

---
+++ src/core/form/fields/ListField.tsx
@@ -0,0 +1,28 @@
+import { useFieldContext } from "../base";
+
+interface Item {
+  label: string;
+  value: string | number;
+}
+
+interface Props {
+  label: string;
+  items: Item[];
+}
+
+const ListField = ({ label }: Props) => {
+  const ctx = useFieldContext<string>();
+  return (
+    <label class="inline-flex flex-col">
+      <span>{label}</span>
+      <input
+        type="text"
+        className="px-3 py-1.5 border border-amber-400"
+        value={ctx.state.value}
+        onInput={(e) => ctx.handleChange(e.currentTarget.value)}
+      />
+    </label>
+  );
+};
+
+export default ListField;
--- src/core/form/output.ts
+++ src/core/form/output.ts
@@ -2,6 +2,7 @@ import { createFormHook } from "@tanstack/preact-form";
 import { fieldContext, formContext } from "./base";
 import TextField from "./fields/TextField";
 import DateField from "./fields/DateField";
+import ListField from "./fields/ListField";
 
 export const { useAppForm } = createFormHook({
   fieldContext,
@@ -9,6 +10,7 @@ export const { useAppForm } = createFormHook({
   fieldComponents: {
     TextField,
     DateField,
+    ListField,
   },
   formComponents: {},
 });
