224400f80b4c1e0735e80d896d26697a12530bf3
Herob527 <szymon.wrzos@onet.pl>
2026-09-19 15:12:05 +0200

    Implement ListField

--- src/core/form/fields/ListField.tsx
+++ src/core/form/fields/ListField.tsx
@@ -19,30 +19,72 @@ interface Props {
 // />
 const ListField = ({ label, items }: Props) => {
   const ctx = useFieldContext<(string | number)[]>();
+
+  const handleChange = (value: string | number) => {
+    const isEmpty =
+      ctx.state.value?.length === undefined || ctx.state.value?.length === 0;
+    if (isEmpty) {
+      return ctx.handleChange([value]);
+    }
+    const alreadyHas = ctx.state.value.includes(value);
+    if (alreadyHas) {
+      return ctx.handleChange(ctx.state.value.filter((item) => item !== value));
+    }
+    ctx.handleChange([...ctx.state.value, value]);
+  };
+
+  const display = (() => {
+    if (
+      ctx.state.value?.length === undefined ||
+      ctx.state.value?.length === 0
+    ) {
+      return "Pick something";
+    }
+    return ctx.state.value
+      .map((it) => items.find((item) => item.value === it)?.label)
+      .join(", ");
+  })();
+
   return (
-    <label className="inline-flex flex-col">
+    <>
       <span>{label}</span>
       <Popover.Root>
-        <Popover.Trigger>
-          <span>Pick</span>
+        <Popover.Trigger asChild>
+          <button
+            className="border border-amber-400"
+            aria-label="Update dimensions"
+          >
+            <span>{display}</span>
+          </button>
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content>
-            <div>
-              {items.map((it) => (
-                <div key={it.value} className={it.label}>
-                  <Checkbox.Root
-                    checked={ctx.state?.value?.includes(it.value) ?? false}
-                  >
-                    <Checkbox.Indicator />
-                  </Checkbox.Root>
-                </div>
-              ))}
+          <Popover.Content className="PopoverContent" sideOffset={5}>
+            <div className="bg-white border border-amber-400 rounded-sm px-4 py-2">
+              <Popover.Close className="PopoverClose" aria-label="Close">
+                X
+              </Popover.Close>
+              <div className="flex flex-col gap-2">
+                {items.map((item) => (
+                  <div className="flex items-center gap-2">
+                    <Checkbox.Root
+                      key={item.value}
+                      value={item.value}
+                      className="bg-white h-8 w-8 rounded-sm flex items-center justify-center border border-amber-400"
+                      onCheckedChange={() => handleChange(item.value)}
+                      checked={ctx.state.value?.includes(item.value) ?? false}
+                    >
+                      <Checkbox.Indicator>X</Checkbox.Indicator>
+                    </Checkbox.Root>
+                    <label>{item.label}</label>
+                  </div>
+                ))}
+              </div>
             </div>
+            <Popover.Arrow className="PopoverArrow" />
           </Popover.Content>
         </Popover.Portal>
       </Popover.Root>
-    </label>
+    </>
   );
 };
 
